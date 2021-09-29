0b1563dcde27fa27ef5f5b79682790bda57120db
milena <pustet@posteo.de>
2021-09-29 13:11:57 +0200

    clean up

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,8 @@
 import { PlantService } from '../service/db.sqlqueries.js';
 
-//prinzipielle Idee des Controllers ist, neben dem Aufruf der jeweiligen Funktion von PlantService den Funktionsaufruf so vorzubereiten, dass die entsprechenden Daten korrekt und vollständig übergeben werden. Außerdem werden die resultate der Datenbank jeweils als JSON zurückgesendet
-
 export const Controller = {
 
-    readAll: (req, res) => {  //ruft die create()-Funktion des PlantService auf. Dazu muss jedoch erst der Datensatz erstellt werden, der in die Datenbank eingefügt werden soll. Dieser Datensatz wird aus dem body des requests extrahiert
+    readAll: (req, res) => {
         PlantService.getAll((err, result) => {
             if (err)
                 res.status(500).send({
@@ -16,7 +14,6 @@ export const Controller = {
                 result.forEach( plant => {
                     let buff = new Buffer(plant.image, 'base64');
                     let text = buff.toString('ascii');
-                    //console.log(text);
                     plant.image = text;
                     arr.push(plant);
                 });
@@ -40,44 +37,4 @@ export const Controller = {
             else res.json(result);
         });
     },
-
-    readOne: (req, res) => {
-        PlantService.findById(req.params.plantId, (err, result) => {
-            if (err)
-                res.status(500).send({
-                    message: err.message || "Some error occurred while getting one post",
-                });
-            else res.json(result);
-        });
-    },
-
-/*    delete: (req, res) => {
-        PlantService.remove(req.params.plantId, (err, result) => {
-            if (err)
-                res.status(500).send({
-                    message: err.message || "Some error occurred while delete the post",
-                });
-            else res.json(result);
-        });
-    },*/
-
-/*    update: (req, res) => {
-        if (!req.body) {
-            res.status(400).send({
-                message: "Content can not be empty!",
-            });
-        }
-        const plant = {...req.body };
-        PlantService.updateById(
-            req.params.plantId,
-            plant,
-            (err, result) => {
-                if (err)
-                    res.status(500).send({
-                        message: err.message || "Some error occurred while update the post",
-                    });
-                else res.json(result);
-            }
-        );
-    },*/
 };
--- service/db.sqlqueries.js
+++ service/db.sqlqueries.js
@@ -13,37 +13,4 @@ export const PlantService = {
             else result(null, res);
         });
     },
-    findById: async(plantId, result) => {
-        sql.query(
-            `SELECT * FROM plants WHERE id = ?`, [plantId],
-            (err, res) => {
-                if (err) result(err, null);
-                else if (res.length) result(null, res[0]);
-                else result({ message: "post not found" }, null);
-            }
-        );
-    },
-/*    updateById: async(id, post, result) => {
-        sql.query(
-            "UPDATE plants SET ? where id= ?", [post, id],
-            (err, res) => {
-                if (err) result(null, err);
-                else if (res.affectedRows == 0) result({ message: "post not found" }, null);
-                else result(null, { id: id, ...post });
-            }
-        );
-    },*/
-/*    remove: async(id, result) => {
-        sql.query("DELETE FROM plants WHERE id = ?", id, (err, res) => {
-            if (err) result(null, err);
-            else if (res.affectedRows == 0) result({ message: "data not found" }, null);
-            else result(null, res);
-        });
-    },*/
-/*    removeAll: async(result) => {
-        sql.query("DELETE FROM plants", (err, res) => {
-            if (err) result(null, err);
-            else result(null, res);
-        });
-    },*/
 };
